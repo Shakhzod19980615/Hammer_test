9cecac8e5043c1591f59ee04de24315b0e00b5ec
shakhzod_bakhromov <shakhzod_bakhromov@mail.ru>
2023-11-09 09:30:42 +0000

    offline regime

--- app/src/main/java/com/example/hammer_test/data/AppDatabase.kt
+++ app/src/main/java/com/example/hammer_test/data/AppDatabase.kt
@@ -1,11 +1,12 @@
 package com.example.hammer_test.data
-
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.hammer_test.data.bannerDataSource.BannerItemDao
 import com.example.hammer_test.data.bannerDataSource.BannerItemEntity
 
+
 @Database(entities = [BannerItemEntity::class],version = 2, exportSchema = true)
+
 abstract class AppDatabase : RoomDatabase() {
     abstract val bannerItemDao: BannerItemDao
 }
